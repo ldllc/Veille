07f4b43f1ebff8fb5960dc6d872478bc0eedcb12
ldllc <115065483+ldllc@users.noreply.github.com>
2024-02-20 11:27:12 +0100

    Update CSV_Attaques.py

--- Scripts/Attaques/CSV_Attaques.py
+++ Scripts/Attaques/CSV_Attaques.py
@@ -1,11 +1,7 @@
 import pandas as pd
-from datetime import datetime
 
 file_ransomfeed = "../../CSV/Attaques/Donnees_Attaques/Ransomfeedit.csv"
 file_ransomwarelive = "../../CSV/Attaques/Donnees_Attaques/Ransomwarelive.csv"
-output_file = "../../CSV/Attaques/Base_Attaques/Attaques.csv"
-
-existing_df = pd.read_csv(output_file, sep='|')
 
 df_ransomfeed = pd.read_csv(file_ransomfeed, sep='|')
 df_ransomwarelive = pd.read_csv(file_ransomwarelive, sep='|')
@@ -15,20 +11,10 @@ merged_df.insert(0, 'ID', range(1, 1 + len(merged_df)))
 merged_df = merged_df.rename(columns={'post_title': 'Name', 'group_name': 'Group'})
 merged_df = merged_df.fillna("Not provided")
 
-today_attacks = merged_df[merged_df['discovered'] == datetime.today().strftime('%Y-%m-%d')]
+final_df = merged_df[['ID', 'Name', 'Group', 'discovered', 'published', 'country', 'description', 'website', 'post_url']]
+final_df = final_df.drop_duplicates(subset=['Name', 'Group', 'discovered'])
+final_df = final_df.sort_values(by='discovered', ascending=False)
+final_df = final_df.reset_index(drop=True)
 
-today_attacks = today_attacks.groupby(['Name', 'Group'], as_index=False).agg({
-    'discovered': 'max',
-    'published': 'max',
-    'country': ', '.join,
-    'description': ', '.join,
-    'website': ', '.join,
-    'post_url': ', '.join  
-})
-
-existing_df = pd.concat([existing_df, today_attacks], ignore_index=True)
-existing_df = existing_df.drop_duplicates(subset=['Name', 'Group'])
-existing_df = existing_df.sort_values(by='discovered', ascending=False)
-existing_df = existing_df.reset_index(drop=True)
-existing_df['ID'] = existing_df['ID'].astype(int)
-existing_df.to_csv(output_file, index=False, sep='|')
+output_file = "../../CSV/Attaques/Base_Attaques/Attaques.csv"
+final_df.to_csv(output_file, index=False, sep='|')
